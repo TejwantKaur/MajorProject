eaa6c36a1cdbd5cea9791cb44c05a3faa094678c
TejwantKaur <kaurtejwant998@gmail.com>
2024-02-17 15:01:37 +0530

    session

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const Listing = require("./models/listings.js");
 const path = require("path");
 const methodOverride = require("method-override")
 const ejsMate = require("ejs-mate");
-const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js");
-const { listingSchema, reviewSchema } = require("./schema.js");
-const Review = require("./models/review.js");
+const session = require("express-session");
+const flash = require("connect-flash");
+
+const listings = require("./routes/listing.js");
+const reviews = require("./routes/review.js");
 
 app.set("view engine", "ejs");
 app.set("views",path.join(__dirname,"views"));
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.engine("ejs", ejsMate);
 app.use(express.static(path.join(__dirname,"/public")));
@@ -31,108 +32,30 @@ async function main(){
     await mongoose.connect(mongoURL);
 }
 
+const sessionOptions = {
+    secret: "mySuperSecretKey",
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
+        maxAge: 7 * 24 * 60 * 60 * 1000,
+    },
+};
+
 app.get("/", (req,res)=>{
     res.send("Hi i am root");
-})   
+}); 
 
-const validateListing = (req,res,next) => {
-    let { error } = listingSchema.validate(req.body);
-    if(error){
-        let errMsg = error.details.map((el)=>el.message).join(",");
-        throw new ExpressError(400,errMsg);
-    } else {
-        next();
-    }
-};
-const validateReview = (req,res,next)=>{
-    let { error } = reviewSchema.validate(req.body);
-    if(error){
-        let errMsg = error.details.map((el)=>el.message).join(",");
-        throw new ExpressError(400,errMsg);
-    } else {
-        next();
-    }
-}
+app.use(session(sessionOptions));
+app.use(flash());
 
-// Index Route
-app.get(
-    "/listings", 
-    wrapAsync(async(req,res,next)=>{
-    const allListings = await Listing.find({});
-    res.render("listings/index.ejs", {allListings});
-}));
-
-app.get("/listings/new", (req,res)=>{
-    res.render("listings/new.ejs");
+app.use((req,res,next) => {
+    res.locals.success = req.flash("success");
+    next();
 });
 
-// Create Route
-app.post(
-    "/listings",
-    validateListing,
-    wrapAsync(async(req,res,next) => {
-        const newListing = new Listing (req.body.listing);
-        await newListing.save();
-        res.redirect("/listings");
-    })
-);
-
-// Show Route
-app.get(
-    "/listings/:id", 
-    wrapAsync(async(req,res,next)=>{
-    let { id } = req.params;
-    const listing = await Listing.findById(id).populate("reviews"); 
-    // console.log(listing)
-    res.render("listings/show.ejs", { listing });
-}));
-
-app.get(
-    "/listings/:id/edit",
-    wrapAsync(async (req,res,next)=>{
-    let { id } = req.params;
-    const listing = await Listing.findById(id)
-    res.render("listings/edit.ejs", { listing })
-}));
-
-// Update Route
-app.put(
-    "/listings/:id",
-    validateListing,
-    wrapAsync(async(req,res,next)=>{
-    let { id } = req.params;
-    await Listing.findByIdAndUpdate(id, {...req.body.listing});
-    res.redirect(`/listings/${id}`);
-}));
-
-// Delete Route
-app.delete(
-    "/listings/:id",
-    wrapAsync(async(req,res,next)=>{
-    let { id } = req.params;
-    let deletedListing = await Listing.findByIdAndDelete(id);
-    console.log(deletedListing);
-    res.redirect("/listings");
-}));
-
-// Reviews
-// Post Route
-app.post("/listings/:id/reviews", 
-    validateReview,
-    wrapAsync(async(req,res)=>{
-        console.log(req.body)
-    let listing = await Listing.findById(req.params.id);
-
-    let newReview = new Review(req.body.reviews);
-    listing.reviews.push(newReview);
-
-    await newReview.save();
-    await listing.save();
-    console.log("new Review Saved");
-    console.log(listing.reviews)
-    // res.send("Saved!!")
-    res.redirect(`/listings/${ listing._id }`);
-})); 
+app.use("/listings", listings);
+app.use("/listings/:id/reviews", reviews)
 
 // for Rest_all
 app.all("*", (req,res,next)=>{
--- models/listings.js
+++ models/listings.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const Review = require("./review.js");
 
 defImg = "https://images.unsplash.com/photo-1483794344563-d27a8d18014e?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
 
@@ -24,6 +25,12 @@ const listingSchema = new Schema({
             ref: "Review"
         }
     ]
+});
+
+listingSchema.post("findOneAndDelete", async (listing) => {
+    if(listing){
+        await Review.deleteMany({ _id: { $in: listing.reviews }});
+    }
 });
 
 // collection
