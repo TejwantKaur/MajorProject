0e9bac5f031bc2e8c6680e3955e06e40ed996866
TejwantKaur <kaurtejwant998@gmail.com>
2024-02-21 21:50:47 +0530

    mvc

--- routes/listing.js
+++ routes/listing.js
@@ -6,8 +6,14 @@ const { isLoggedIn, isOwner, validateListing } = require("..//middleware.js");
 const Listing = require("../models/listings.js");
 const lisitngController = require("../controllers/listing.js")
 
-// Index Route
-router.get("/", wrapAsync(lisitngController.index));
+router
+    .route("/")
+    .get(wrapAsync(lisitngController.index))
+    .post(
+        isLoggedIn,
+        validateListing,
+        wrapAsync(lisitngController.createListing)
+    );
 
 // new route
 router.get("/new", 
@@ -15,36 +21,26 @@ router.get("/new",
     lisitngController.renderNewForm
 );
 
-// Show Route
-router.get("/:id", wrapAsync(lisitngController.showListing));
+router
+    .route("/:id")
+    .get(wrapAsync(lisitngController.showListing))
+    .put(
+        isLoggedIn,
+        isOwner,
+        validateListing,
+        wrapAsync(lisitngController.editListing),
+    )
+    .delete(
+        isLoggedIn,
+        isOwner,
+        wrapAsync(lisitngController.destroyListing)
+    );
 
-// Create Route
-router.post("/",
-    isLoggedIn,
-    validateListing,
-    wrapAsync(lisitngController.createListing)
-);
-
-// Edit route
-router.get("/:id/edit",
+router
+.get("/:id/edit",
     isLoggedIn,
     isOwner,
     wrapAsync(lisitngController.renderEditForm)
-);
-
-// Update Route
-router.put("/:id",
-    isLoggedIn,
-    isOwner,
-    validateListing,
-    wrapAsync(lisitngController.editListing),
-);
-
-// Delete Route
-router.delete("/:id",
-    isLoggedIn,
-    isOwner,
-    wrapAsync(lisitngController.destroyListing)
 );
 
 module.exports = router;
--- routes/review.js
+++ routes/review.js
@@ -1,55 +1,25 @@
 const express = require("express");
 const router = express.Router({ mergeParams: true});
 const wrapAsync = require("../utils/wrapAsync.js");
-const ExpressError = require("../utils/ExpressError.js");
 
 const Listing = require("../models/listings.js");
 const Review = require("../models/review.js");
 const { validateReview, isLoggedIn, isReviewAuthor } = require("../middleware");
 
-// Post
-// Review Route
+const reviewController = require("../controllers/reviews");
+
+// Post Review Route
 router.post("/",
     isLoggedIn,
     validateReview,
-
-    wrapAsync(async(req,res)=>{
-        console.log(req.body)
-        
-        let listing = await Listing.findById(req.params.id);
-        
-        let newReview = new Review(req.body.review);
-        newReview.author = req.user._id;
-        console.log(newReview);
-        listing.reviews.push(newReview);
-
-        await newReview.save();
-        await listing.save();
-        console.log("new Review Saved");
-        req.flash("success", "newReview Posted!");
-        // console.log(listing.reviews)
-        // res.send("Saved!!")
-        res.redirect(`/listings/${ listing._id }`);
-    }),
+    wrapAsync(reviewController.createReview),
 ); 
-
 
 // Delete Review Route
 router.delete("/:reviewId",
     isLoggedIn,
     isReviewAuthor,
-    wrapAsync(async(req,res)=>{
-        let { id, reviewId } = req.params;
-
-        await Listing.findByIdAndUpdate(id,{ 
-            $pull: {
-                reviews: reviewId
-            }});
-        
-        await Review.findByIdAndDelete(reviewId);
-        req.flash("success", "Listing Deleted!");
-        res.redirect(`/listings/${id}`);
-    }),
+    wrapAsync(reviewController.destroyReview),
 );
 
 module.exports = router;
